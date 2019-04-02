5ca119ffb8bc885be5bf1111636912788e105aac
Dustin Herboldshimer and Justin Kerntz <dustin@>
2019-04-02 10:31:37 -0700

    add puzzle input,output, and logic

--- js/scripts.js
+++ js/scripts.js
@@ -17,4 +17,27 @@ $(document).ready(function()  {
       return outputCount;
     }else{alert("Numbers must be greater than 0, and Count To must be larger than Count By.")}
   };
+// Start of Wordpuzzle logic
+function deVowel(list){
+  var vowels = ['a','e','i','o','u','A','E','I',"O",'U'];
+
+  vowels.forEach(function(vowel){
+    for (var i = 0; i < list.length; i++){
+      if (vowel === list[i]){
+        list.splice(i,1, "-");
+      };
+    };
+  });
+  return list;
+};
+var testArray = ['t','g','a','b','e'];
+console.log(deVowel(testArray));
+
+  $('#puzzleSubmit').click(function() {
+    var userInput = $('#puzzleInput').val();
+    var inputSplit = userInput.split('');
+    console.log(inputSplit);
+    $('#puzzleOutPut').append(deVowel(inputSplit));
+
+  })
 })
