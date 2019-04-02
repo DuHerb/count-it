a64a892e251488bd4d78e21e812edb000191bf94
Dustin Herboldshimer and Justin Kerntz <justin@>
2019-04-02 13:59:15 -0700

    add fortune.html and logic to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function()  {
     // console.log(countTo);
     // console.log(countBy);
     var outputCount = countIt(countTo,countBy);
-    console.log(outputCount);
+    // console.log(outputCount);
   })
   function countIt(countTo,countBy){
     var outputCount = [];
@@ -31,7 +31,7 @@ function deVowel(list){
   return list;
   };
   var testArray = ['t','g','a','b','e'];
-  console.log(deVowel(testArray));
+  // console.log(deVowel(testArray));
 
   $('#puzzleSubmit').click(function() {
     var userInput = $('#puzzleInput').val();
@@ -40,6 +40,7 @@ function deVowel(list){
     $('#puzzleOutPut').append(deVowel(inputSplit));
   })
 //Start of TransportationQuiz
+
   $("#quizSubmit").click(function() {
     $("input:checkbox[name=work]:checked").each(function(){
       var workMode = $(this).val();
@@ -51,6 +52,36 @@ function deVowel(list){
       $("#quizResults").append("<li>" + notWorkMode + "</li>");
     });
     $("form").hide();
+  });
 
+  //Start of Fortune =------------------------------------------------
+  var allLuck = "Look at the Golden Horeseshoes on this Guy! You're the luckiest sum' bitch I ever seen!<br />";
+  var noLuck = "Don't look up.  There's probably a rock from space hurtling towards your face.<br />";
+  var littleLuck = "Look on the bright side, things can only get better from here.<br />"
+  var mostlyLuck = "Start playing the lotto.  Two dollar payouts gauranteed.  Sometimes.<br />"
+  var wholeLottaLuck = "That levy is never gonna break. All the lemons you can squeeze.<br />"
+
+  $('#fortuneSubmit').click(function(){
+    var luckIndex = 0;
+
+    if ($('#gizmo').is(":checked")){
+      $('#fortuneOutput').append("You have adopted Gizmo and unwittingly unleashed Gremlins and destroyed your family's home.  Way to go.");
+    } else {
+      $('input:checkbox[name=fortune]:checked').each(function(){
+        luckIndex += parseInt($(this).val());
+        console.log(luckIndex);
+      });
+    if (luckIndex === -6) {
+          $('#fortuneOutput').append(noLuck);
+      } else if (luckIndex <= -3 ){
+        $('#fortuneOutput').append(littleLuck);
+      } else if (luckIndex <= 2 ) {
+        $('#fortuneOutput').append(mostlyLuck);
+      } else if (luckIndex < 5){
+        $('#fortuneOutput').append(wholeLottaLuck);
+      } else {
+        $('#fortuneOutput').append(allLuck);
+    };
+  };
   });
 });
